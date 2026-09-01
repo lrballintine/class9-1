8348d801ad76595df46c22da08e22c26d054645b
Luke Ballintine <lukeballintine@Lukes-MacBook-Pro-2.local>
2026-09-01 10:44:18 -0500

    Count

--- Reasources/Scripts/index.js
+++ Reasources/Scripts/index.js
@@ -1,5 +1,12 @@
+let count = 0
 
 function handleOnLoad(){
     document.getElementById("app").innerHTML=`
-    <h1>Hello World</h1>`
+    <div id = counter><h1>Hello World</h1></div>
+    <button id = "myButton">Click Me </button>`
+    btn = document.getElementById("myButton")
+    btn.addEventListener("click", function () {
+    count++
+    document.querySelector("#counter").innerHTML = `<h1>You Have Clicked ${count} times </h1>`
+    })
 }
